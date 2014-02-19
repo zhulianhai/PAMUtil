d595c3c6e534117d9ae157dec31a8f1ab248db64
Takafumi Tamura <taktamur@gmail.com>
2014-02-19 13:20:04 +0900

    Initial Commit

---
+++ PAMUtil/PAMViewController.h
@@ -0,0 +1,13 @@
+//
+//  PAMViewController.h
+//  PAMUtil
+//
+//  Created by tak on 2014/02/19.
+//  Copyright (c) 2014年 taktamur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PAMViewController : UIViewController
+
+@end
---
+++ PAMUtil/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PAMUtil
+//
+//  Created by tak on 2014/02/19.
+//  Copyright (c) 2014年 taktamur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PAMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PAMAppDelegate class]));
+    }
+}
